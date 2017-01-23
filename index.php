db5c8399e6f53eab3f19362b27b8d56e17d389d2
IvesMatheus <ives_matheus@hotmail.com>
2017-01-23 14:13:13 -0400

    Commit inicial

---
+++ index.php
@@ -0,0 +1,51 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8"/>
+        <title>LIBRAS</title>
+        <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    </head>
+    <body>
+        <header id="cabecalho">
+                <figure id="logo">
+                    <img src=""/>
+                    <figcaption></figcaption>
+                </figure>
+                <nav id="menu">
+                    <ul id="opcoes">
+                        <li><a href="index.php">Sobre LIBRAS</a></li>
+                        <li><a href="index.php">Sinais</a></li>
+                        <li><a href="index.php">Projetos para LIBRAS</a></li>
+                    </ul>
+                </nav>
+
+            <h1>GLOSSÁRIO DE LIBRAS</h1>
+        </header>
+        <section id="sobre">
+            <h1>APRESENTAÇÃO</h1>
+            <p>
+                Esse site tem o intuito de servir como uma ferramenta para aprendizagem de LIBRAS. O objetivo é alcançar pessoas que estão entrando no universo dos surdos/mudos e buscam um canal para começar ou aumentar seu vocabulário em LIBRAS. Acessando o glossário, está disponível vários vídeos apresentando sinais. Os vídeos estão dividos em categorias como "cores", "alimentos" e "saudações".
+            </p>
+        </section>
+        <section id="exemplos">
+            <figure>
+                <img src="img/boa_noite.jpg" />
+                <figcaption><h1>Boa Noite</h1></figcaption>
+            </figure>
+            <figure>
+                <img src="img/almocar.jpg"/>
+                <figcaption><h1>Almoçar</h1></figcaption>
+            </figure>
+            <figure>
+                <img src="img/euteamo.jpg"/>
+                <figcaption><h1>Eu te Amo</h1></figcaption>
+            </figure>
+        </section>
+        <section id="projetos_libras">
+            <h1>PROJETOS COM LIBRAS</h1>
+            <figure>
+                <img src="" />
+                <img src="" />
+            </figure>
+        </section>
+    </body>
+</html>
